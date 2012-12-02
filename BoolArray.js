e5672b6188cb1af57761b9cc11c0b60c980a8772
Jesús Leganés Combarro "Piranna" <piranna@gmail.com>
2012-12-02 11:50:00 +0100

    Removed validation warnings (added semicolons)

--- BoolArray.js
+++ BoolArray.js
@@ -5,41 +5,41 @@
 
 function BoolArray(length)
 {
-  ArrayBufferView.call(this, Math.ceil(length/8))
+  ArrayBufferView.call(this, Math.ceil(length/8));
 
 
   this.get = function(index)
   {
     if(index >= length)
-      throw INDEX_SIZE_ERR
+      throw INDEX_SIZE_ERR;
 
-    var i = Math.ceil(index/8)
-    var j = index%8
+    var i = Math.ceil(index/8);
+    var j = index%8;
 
-    return (this[i] << (7-j)) >> 7
-  }
+    return (this[i] << (7-j)) >> 7;
+  };
 
   this.set = function(index, value)
   {
     if(index >= length)
-      throw INDEX_SIZE_ERR
+      throw INDEX_SIZE_ERR;
 
-    var i = Math.ceil(index/8)
-    var j = index%8
+    var i = Math.ceil(index/8);
+    var j = index%8;
 
     if(value)
-      this[i] |= 1 << j
+      this[i] |= 1 << j;
     else
-      this[i] &= ~(1 << j)
-  }
+      this[i] &= ~(1 << j);
+  };
 
 
   this.__defineGetter__("length", function()
   {
-    return length
-  })
+    return length;
+  });
 }
 
-BoolArray.prototype = new ArrayBufferView()
+BoolArray.prototype = new ArrayBufferView();
 
-BoolArray.prototype.BYTES_PER_ELEMENT = 0.125
+BoolArray.prototype.BYTES_PER_ELEMENT = 0.125;
